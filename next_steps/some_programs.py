acbc1c51894f934c67aaabe289bb9226a72e8069
Anushree Kumar <112722298+silentswag@users.noreply.github.com>
2023-09-20 16:53:55 +0530

    successful server update from local git bash on next steps

--- next_steps/some_programs.py
+++ next_steps/some_programs.py
@@ -4,7 +4,9 @@ app = Dash(__name__)
 
 app.layout = html.Div([
     html.Div(children='Hello World'),
-    html.Div(children=" my first access and explore to git gitlab git bash")
+    html.Div(children=" my first access and explore to git gitlab git bash"),
+    html.Div(children="successful remote server updates from local machine")
+    
 ])
 
 if __name__ == '__main__':
