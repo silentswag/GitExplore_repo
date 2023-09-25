0e92697282e7d3e44d6bae106c6308cfdb1b3c62
AnushreeKumar <112722298+silentswag@users.noreply.github.com>
2023-09-25 13:00:53 +0530

    "max sum combi"

--- next_steps/Geeks_for_Geeks/arrays.py
+++ next_steps/Geeks_for_Geeks/arrays.py
@@ -1,3 +1,8 @@
+#Return the maximum K valid sum combinations from all the possible sum combinations.
 a=[1,2,8,7]
-for i in range(a):
-    print(a[i])
+b=[4,7,8,2]
+for i in range(len(a)):
+    list=[]
+    for j in range(len(b)):
+        list.append(a[i]+b[j])
+    print(max(list))
